211ff019bfde4c717a77bae05bda58e8205e12da
a1ohomora <kolskyrozz@gmail.com>
2022-09-12 16:22:34 +0300

    feat:add launch with argument

---
+++ src/test/java/ru/rozvezev/GroupingAppTest.java
@@ -0,0 +1,20 @@
+package ru.rozvezev;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class GroupingAppTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
